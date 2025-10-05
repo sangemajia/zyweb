f894d41db16ddffc0d8101c7f9b193e0b85e727f
ZyWeb Developer <developer@example.com>
2025-10-05 22:58:16 +0800

    Update database service exports for Web environment

--- src/main/core/db/service/index.web.ts
+++ src/main/core/db/service/index.web.ts
@@ -1,6 +1,4 @@
-// Web 环境下的数据库服务集合
-import { drive, setting } from '../../server/routes/v1/drive/db.service.web';
-import { history, site, analyze, iptv } from '../../server/routes/v1/history/db.service.web';
+// 在Web环境中，我们导出模拟的服务
+import { analyze, drive, history, setting, iptv, channel, star, site, db } from '../../server/routes/v1/drive/db.service.web';
 
-// 为了保持接口一致性，我们导出相同的对象
-export { drive, setting, history, site, analyze, iptv };
+export { history, setting, star, site, iptv, channel, analyze, drive, db };
